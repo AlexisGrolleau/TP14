a44e08006631d5cd86149a9487f52a8dca47a871
AlexisGrolleau <alexis.grolleau@isen-ouest.yncrea.fr>
2020-05-13 17:02:39 +0200

    oui

--- Exercice3.py
+++ Exercice3.py
@@ -1,14 +1,27 @@
-from PySide2.QtWidgets import QTextEdit,QGridLayout,QLabel,QWidget,QPushButton,QApplication,QVBoxLayout,QDateEdit
-from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
 
 class Window(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.setWindowTitle("IHM")
         self.setFixedSize(400,300)
-        myicon = QIcon.addFile("fr-flag.png")
-        self.setWindowIcon(myicon)
+        self.myicon = QIcon("fr-flag.png")
+        self.setWindowIcon(self.myicon)
         layout = QVBoxLayout()
+        label = QLabel("Hello World")
+        label.setAlignment(Qt.AlignCenter)
+        progressbar = QProgressBar()
+        progressbar.setValue(50)
+        lineedit = QLineEdit()
+        clickme = QPushButton("Click me !")
+        clickme.setToolTip("PLEASE")
+        layout.addWidget(label)
+        layout.addWidget(progressbar)
+        layout.addWidget(lineedit)
+        layout.addWidget(clickme)
+        self.setLayout(layout)
+
 
 
 if __name__ == "__main__":
